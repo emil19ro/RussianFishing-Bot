933733aec0316897557c514ed8a3b035353d0637
emil19ro <emil19ro@gmail.com>
2024-05-11 17:07:00 +0200

    install python to path

--- install.bat
+++ install.bat
@@ -1,15 +1,15 @@
 @echo off
-echo Descarcă Python 3.1.0
+echo Descarcă Python 3.10.0
 curl -o python310.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
 
-echo Instalează Python 3.1.0
-start /wait python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+echo Instalează Python 3.10.0
+start /wait python310.exe /quiet TargetDir=C:\Python310 InstallAllUsers=1 PrependPath=1
 
 echo Instalează PyAutoGUI și Keyboard
-pip install pyautogui keyboard
+C:\Python310\python.exe -m pip install pyautogui keyboard
 
 echo Instalează OpenCV-Python
-pip install opencv-python
+C:\Python310\python.exe -m pip install opencv-python
 
 echo Curăță fișierele temporare
 del python310.exe
